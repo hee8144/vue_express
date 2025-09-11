e2690bb19699123e11dec20964c2742a39004dbc
hee <zcgy432@gmail.com>
2025-09-11 18:18:19 +0900

    2025-09-11

--- server/server.js
+++ server/server.js
@@ -149,9 +149,11 @@ app.get('/prof/list', async (req, res) => {
       });
       return obj;
     });
+    const count = await connection.execute(`SELECT COUNT(*) FROM PROFESSOR `);
     res.json({
         result : "success",
-        profList : rows
+        profList : rows,
+        count : count.rows[0][0]
     });
   } catch (error) {
     console.error('Error executing query', error);
@@ -306,6 +308,86 @@ app.get('/prof/update', async (req, res) => {
   } catch (error) {
     console.error('Error executing update', error);
     res.status(500).send('Error executing update');
+  }
+});
+
+app.get('/board/list', async (req, res) => {
+  const { pageSize , offset  } = req.query;
+  
+  try {
+    const result = await connection.execute(
+      `SELECT B.*, TO_CHAR(CDATETIME, 'YYYY-MM-DD') AS CDATE FROM TBL_BOARD B OFFSET ${offset} ROWS FETCH NEXT ${pageSize} ROWS ONLY `
+    );
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    const count = await connection.execute(
+      `SELECT COUNT(*)  FROM TBL_BOARD`
+    );
+
+    
+    // 리턴
+    res.json({
+        result : "success",
+        boardList : rows ,
+        count: count.rows[0][0]
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+app.get('/board/add', async (req, res) => {
+  const { title, contents, userId ,kind } = req.query;
+
+  try {
+    await connection.execute(
+      `INSERT INTO TBL_BOARD VALUES(B_SEQ.NEXTVAL , :title , :contents, :userId, '0' ,'0', :kind , SYSDATE, SYSDATE )`,
+      [title, contents, userId ,kind],
+      { autoCommit: true }
+    );
+    res.json({
+        result : "success"
+    });
+  } catch (error) {
+    console.error('Error executing insert', error);
+    res.status(500).send('Error executing insert');
+  }
+});
+
+app.get('/board/info', async (req, res) => {
+  const {  boardNo } = req.query;
+  
+  try {
+    const result = await connection.execute(
+      `SELECT B.*, TO_CHAR(CDATETIME, 'YYYY-MM-DD') AS CDATE FROM TBL_BOARD B WHERE BOARDNO = ${boardNo}` 
+    );
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    // 리턴
+    res.json({
+        result : "success",
+        info : rows
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
   }
 });
 
